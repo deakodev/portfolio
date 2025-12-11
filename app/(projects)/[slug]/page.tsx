ce2af59dde011c1c2b1eb97adabaaf5ed625ef95
Zachary Deak <deakodev@outlook.com>
2025-12-11 10:23:40 -0500

    refactor: remove sitemap file and update import path for baseUrl in project page

--- app/(projects)/[slug]/page.tsx
+++ app/(projects)/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import { notFound } from "next/navigation";
 import { CustomMDX } from "@/app/components/mdx";
 import { getProjects } from "@/app/(projects)/utils";
-import { baseUrl } from "@/app/sitemap";
+import { baseUrl } from "@/app/config";
 import { Header } from "./header";
 import "./mdx.css";
 
